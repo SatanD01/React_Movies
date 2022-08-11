bf906b2a68a2d1fe16b31dbeab1c57f8f64538d9
Prince of persia Dastan <60606830+SatanD01@users.noreply.github.com>
2022-08-11 21:11:23 +0500

    err log

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -13,7 +13,7 @@ class Main extends Component {
 
     searchMovies = (str, type = 'all') => {
         this.setState({loading: true})
-        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
             .then(response => response.json())
             .then((data) => {
                 this.setState({
@@ -21,16 +21,24 @@ class Main extends Component {
                     loading: false
                 })
             })
+            .catch((err) => {
+                console.error(err);
+                this.setState({loading: false})
+            })
     }
 
     componentDidMount() {
-        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
+        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
             .then(response => response.json())
             .then((data) => {
                 this.setState({
                     movies: data.Search,
                     loading: false
                 })
+            })
+            .catch((err) => {
+                console.error(err);
+                this.setState({loading: false})
             })
     }
 
